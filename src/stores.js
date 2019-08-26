29ef8bc145ca9306f1078fde566e5efc48804162
Michael Symmes <msymmes@gmail.com>
2019-08-26 19:13:56 -0400

    Added new stores.

--- src/stores.js
+++ src/stores.js
@@ -1,6 +1,8 @@
 import { writable } from "svelte/store";
-export const editor = writable("vim");
-export const mode = writable("javascript");
-export const lint = writable("javascript");
-export const width = writable("100%");
+export const editor = writable("");
+export const mode = writable("");
+export const lint = writable("");
+export const width = writable("");
 export const fullscreen = writable(false);
+export const advancedModeOn = writable(false);
+export const editSettings = writable(false);
