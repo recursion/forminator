de76b186182a2783562c0a0acfd5390d8f8d3abe
Michael Symmes <msymmes@gmail.com>
2019-08-26 15:06:08 -0400

    Added sublime default, and import comments.

--- src/config.js
+++ src/config.js
@@ -1,18 +1,41 @@
+// Import CodeMirror addons/extensions
+// modes
 import "codemirror/mode/javascript/javascript";
 import "codemirror/mode/jsx/jsx";
 import "codemirror/mode/css";
+import "codemirror/mode/htmlmixed/htmlmixed";
 
+// keyMaps
 import "codemirror/keyMap/vim";
 import "codemirror/keyMap/sublime";
 import "codemirror/keyMap/emacs";
 
+// addons
 import "codemirror/addon/display/panel";
 import "codemirror/addon/lint/lint";
+
+// linters
 import "codemirror/addon/lint/javascript-lint";
 import "codemirror/addon/lint/css-lint";
-import "codemirror/addon/lint/jsx-lint";
 
 export default function(CodeMirror) {
+  // default setup
+  const default_sublime = {
+    lineNumbers: true,
+    lineWrapping: true,
+    scrollbarStyle: null,
+    mode: "text/x-csrc",
+    mode: "javascript",
+    keyMap: "vim",
+    matchBrackets: true,
+    showCursorWhenSelecting: true,
+    inputStyle: "contenteditable",
+    gutters: ["CodeMirror-lint-markers"],
+    lint: [CodeMirror.lint.javascript],
+    theme: "cobalt"
+  };
+
+  // default vim setup
   const default_vim = {
     lineNumbers: true,
     lineWrapping: true,
@@ -30,9 +53,7 @@ export default function(CodeMirror) {
 
   const lintTypes = {
     javascript: CodeMirror.lint.javascript,
-    html: CodeMirror.lint.html,
-    css: CodeMirror.lint.css,
-    jsx: CodeMirror.lint.jsx
+    css: CodeMirror.lint.css
   };
-  return { lintTypes, default_vim };
+  return { lintTypes, default_vim, default_sublime };
 }
