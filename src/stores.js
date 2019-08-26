57974a61895755ee15935a66b4dd85f1062a6b74
Michael Symmes <msymmes@gmail.com>
2019-08-26 15:07:38 -0400

    Added mode and fullscreen stores.

--- src/stores.js
+++ src/stores.js
@@ -1,4 +1,6 @@
 import { writable } from "svelte/store";
 export const editor = writable("vim");
+export const mode = writable("javascript");
 export const lint = writable("javascript");
 export const width = writable("100%");
+export const fullscreen = writable(false);
