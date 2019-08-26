7becf7b3a245f929fec3d4a5b55787fcf359b6a8
Michael Symmes <msymmes@gmail.com>
2019-08-26 15:05:50 -0400

    Rows, new stores, and more.

--- src/App.svelte
+++ src/App.svelte
@@ -2,20 +2,30 @@
   import { onMount } from "svelte";
   import CodeMirror from "codemirror";
   import Editor from "./Editor.svelte";
-  import { width, lint, editor } from "./stores.js";
+  import { fullscreen, mode, width, lint, editor } from "./stores.js";
   import PowerButton from "./PowerButton.svelte";
   import MaximizeButton from "./MaximizeButton.svelte";
   import UIBar from "./UIBar.svelte";
   import ConfigOption from "./ConfigOption.svelte";
-  const types = ["javascript", "html", "css", "jsx"];
+  const modeTypes = ["javascript", "htmlmixed", "css", "jsx"];
+  const lintTypes = ["javascript", "css", "json"];
   const editors = ["vim", "emacs", "sublime"];
 
   let advancedModeOn = false;
 
+  function getCodeMirrorEl() {
+    return document.querySelector(".CodeMirror");
+  }
+
   const wrapEditor = editor => {
+    const row = document.createElement("div");
+    row.className = "row";
+
     const parent = editor.parentNode;
     const lowerbar = document.querySelector("ui-bar.bottom");
     const container = document.querySelector(".forminator.container");
+    row.appendChild(editor);
+
     if (!container) console.error("Container not found.");
     container.insertBefore(editor, lowerbar);
     parent.appendChild(container);
@@ -24,13 +34,21 @@
   onMount(() => {
     wrapEditor(document.querySelector("#editor"));
   });
+
+  $: if ($fullscreen) {
+    var codeMirror = getCodeMirrorEl();
+    if (codeMirror) {
+      codeMirror.classList.add("CodeMirror-fullscreen");
+    }
+  } else if (!$fullscreen) {
+    var codeMirror = getCodeMirrorEl();
+    if (codeMirror) {
+      codeMirror.classList.remove("CodeMirror-fullscreen");
+    }
+  }
 </script>
 
 <style>
-  .container {
-    margin: 0 auto;
-    padding: 0;
-  }
   .spacer {
     width: 24px;
   }
@@ -39,13 +57,21 @@
     flex-direction: row;
     text-align: right;
     justify-content: flex-end;
+    padding: 1px;
+  }
+  .column.tools {
+    padding: 1px;
+  }
+  div.column {
+    display: flex;
+    align-items: center;
   }
 </style>
 
 <div class="forminator container">
   <UIBar position="top" active={advancedModeOn}>
     {#if advancedModeOn}
-      <div class="column">
+      <div class="column tools">
         <MaximizeButton />
       </div>
     {/if}
@@ -65,7 +91,8 @@
     <UIBar position="bottom" active={advancedModeOn}>
       <div class="column" />
       <div class="column config">
-        <ConfigOption data={types} store={lint} title="Mode" />
+        <ConfigOption data={lintTypes} store={lint} title="Linting" />
+        <ConfigOption data={modeTypes} store={mode} title="Mode" />
         <div class="spacer" />
       </div>
     </UIBar>
