949f8e532462bb622ed976576206eeceb0cbde2a
Michael Symmes <msymmes@gmail.com>
2019-08-26 19:13:15 -0400

    Moved CodeMirror imports into own file.

---
+++ src/codeMirrorImports.js
@@ -0,0 +1,19 @@
+// Import CodeMirror addons/extensions
+// modes
+import "codemirror/mode/javascript/javascript";
+import "codemirror/mode/jsx/jsx";
+import "codemirror/mode/css";
+import "codemirror/mode/htmlmixed/htmlmixed";
+
+// keyMaps
+import "codemirror/keyMap/vim";
+import "codemirror/keyMap/sublime";
+import "codemirror/keyMap/emacs";
+
+// addons
+import "codemirror/addon/display/panel";
+import "codemirror/addon/lint/lint";
+
+// linters
+import "codemirror/addon/lint/javascript-lint";
+import "codemirror/addon/lint/css-lint";
